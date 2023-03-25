1eb7b17bb6d6c8daa7e6464ece5df90c4186a605
xSawk <marcinlukasik1997@gmail.com>
2023-03-25 21:47:45 +0100

    Add pagination

--- src/main/java/pl/lukasik/backend/controller/UserController.java
+++ src/main/java/pl/lukasik/backend/controller/UserController.java
@@ -1,10 +1,11 @@
 package pl.lukasik.backend.controller;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.lukasik.backend.model.UserEntity;
 import pl.lukasik.backend.service.UserService;
 
 import java.io.IOException;
@@ -21,12 +22,23 @@ public class UserController {
     }
 
     @PostMapping("/load")
-    public ResponseEntity<String> loadUsers() {
+    public ResponseEntity<String> loadUsers(String path) {
         try {
-            userService.loadUsersFromFile(".\\data\\examples\\user1.json");
+            userService.loadUsersFromFile(".\\data\\examples\\user3.json");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return ResponseEntity.ok("Users loaded successfully");
     }
+
+    @GetMapping()
+    public Page<UserEntity> getUsers(@PageableDefault(size = 25) Pageable pageable){
+        return userService.getUsers(pageable);
+    }
+
+    @GetMapping("/{name}")
+    public UserEntity getUserByName(@PathVariable String name){
+        return userService.getUserByName(name);
+    }
+
 }
--- src/main/java/pl/lukasik/backend/service/UserService.java
+++ src/main/java/pl/lukasik/backend/service/UserService.java
@@ -1,6 +1,9 @@
 package pl.lukasik.backend.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +13,8 @@ import pl.lukasik.backend.repository.UserRepository;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,7 +33,6 @@ public class UserService {
         ObjectMapper mapper = new ObjectMapper();
         File file = new File(fileName);
         UserList users = mapper.readValue(file, UserList.class);
-
         List<UserEntity> userList = users.getUsers().stream()
                 .map(user -> UserEntity.builder()
                         .name(user.getName())
@@ -36,9 +40,22 @@ public class UserService {
                         .login(user.getLogin())
                         .build())
                 .collect(Collectors.toList());
+        Instant start = Instant.now();
         userRepository.saveAll(userList);
+        Instant end = Instant.now();
+        Duration duration = Duration.between(start, end); // zwrócic obiekt statystyk
+        System.out.println("Inserted " + userList.size() + " users in " + duration.getSeconds() + " seconds");
         }
+
+
+    public Page<UserEntity> getUsers(Pageable pageable) {
+        return userRepository.findAll(pageable);
     }
+
+    public UserEntity getUserByName(String name) {
+        return userRepository.findByName(name);
+    }
+}
 
 
 
